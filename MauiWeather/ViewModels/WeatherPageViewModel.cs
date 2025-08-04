c461c34627bf2ed07ba9454fd456c9e8c7c2f2d4
Abdulmalik Muhammad <abdulmalik.m.m@nuprc.gov.ng>
2025-08-04 19:37:26 +0100

    Obtaining the coordinates from a location

--- MauiWeather/ViewModels/WeatherPageViewModel.cs
+++ MauiWeather/ViewModels/WeatherPageViewModel.cs
@@ -1,7 +1,26 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Diagnostics;
 
 namespace MauiWeather.ViewModels;
 
 public partial class WeatherPageViewModel : ObservableObject
 {
+    [RelayCommand]
+    public async Task Search(string searchText)
+    {
+        Location? location = await GetCoordinatesAsync(searchText);
+    }
+    private async Task<Location?> GetCoordinatesAsync(string address)
+    {
+        IEnumerable<Location> locations = await Geocoding.Default.GetLocationsAsync(address) ?? [];
+        Location? location = locations?.FirstOrDefault();
+
+        if (location is not null)
+        {
+            Debug.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+        }
+
+        return location;
+    }
 }
